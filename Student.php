0c1494f29108b666475a4c6a9562c91772389161
rshellborn <rachel@shellborn.com>
2017-01-11 11:11:34 -0800

    commenting code

--- Student.php
+++ Student.php
@@ -1,6 +1,7 @@
 <?php
 
 class Student {
+    #constructs a student with last name, first name, emails, and grades
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -8,14 +9,17 @@ class Student {
         $this->grades = array();
     }
 
+    #adds an email to student
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    #adds a grade to student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    #calculates average grades
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -23,6 +27,7 @@ class Student {
         return $total / count($this->grades);
     }
 
+    #displays student to screen in formatted style
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
--- index.php
+++ index.php
@@ -1,43 +1,48 @@
 <html>
-<body>
-<?php
-include('Student.php');
+    <body>
+        <?php
+            include('Student.php');
 
-$first = new Student();
-$first->surname = "Doe";
-$first->first_name = "John";
-$first->add_email('home','john@doe.com');
-$first->add_email('work','jdoe@mcdonalds.com');
-$first->add_grade(65);
-$first->add_grade(75);
-$first->add_grade(55);
-$students['j123'] = $first;
+            #adding first student
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
 
-$second = new Student();
-$second->surname = "Einstein";
-$second->first_name = "Albert";
-$second->add_email('home','albert@braniacs.com');
-$second->add_email('work1','a_einstein@bcit.ca');
-$second->add_email('work2','albert@physics.mit.edu');
-$second->add_grade(95);
-$second->add_grade(80);
-$second->add_grade(50);
-$students['a456'] = $second;
+            #adding second student
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
 
-$third = new Student();
-$third->surname = "Shellborn";
-$third->first_name = "Rachel";
-$third->add_email('home','rachel@shellborn.com');
-$third->add_grade(96);
-$third->add_grade(89);
-$third->add_grade(94);
-$students['z274'] = $third;
+            #adding third student (myself)
+            $third = new Student();
+            $third->surname = "Shellborn";
+            $third->first_name = "Rachel";
+            $third->add_email('home','rachel@shellborn.com');
+            $third->add_grade(96);
+            $third->add_grade(89);
+            $third->add_grade(94);
+            $students['z274'] = $third;
 
-ksort($students);
+            #sorting students by key
+            ksort($students);
 
-foreach($students as $student) {
-    echo $student->toString();
-}
-?>
-</body>
+            #displaying each student to screen
+            foreach($students as $student) {
+                echo $student->toString();
+            }
+        ?>
+    </body>
 </html>
